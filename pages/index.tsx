5c07f54ab19c65e3b036e4eb728a3bff309a5df7
Catalin <IancuCatalinCristian94@gmail.com>
2022-10-29 20:50:29 +0300

    Cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,6 @@ import { GetServerSideProps } from "next";
 import React, { FC } from "react";
 import Home from "../domains/home/home";
 
-//type Props = { depositData: Deposit[] };
-
 type Props = {
   depositData: Deposit[];
   withdrawalData: Withdrawal[];
