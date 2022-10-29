fef67e88256896cdc1a99b4fe7fc6309e6a75e83
Catalin <IancuCatalinCristian94@gmail.com>
2022-10-29 20:33:34 +0300

    Implemented dropdown functionality

--- domains/home/home.tsx
+++ domains/home/home.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from "../../styles/Home.module.css";
 import { Deposit } from "../shared/models/Deposit";
 import { TradeOrder } from "../shared/models/TradeOrder";
@@ -6,6 +6,7 @@ import { Withdrawal } from "../shared/models/Withdrawal";
 import HomeDeposit from "./components/homeDeposit/homeDeposit";
 import HomeWithdrawal from "./components/homeWithdrawal/homeWithdrawal";
 import HomeTradeOrder from "./components/homeTradeOrder/homeTradeOrder";
+import "bootstrap/dist/css/bootstrap.css";
 
 type Props = {
   deposit: Deposit[];
@@ -14,12 +15,26 @@ type Props = {
 };
 
 const Home: FC<Props> = ({ deposit, withdrawal, tradeOrder }) => {
+  const [selectedOption, setSelectedOption] = useState("1");
   return (
     <div className={styles.container}>
-      <main>
-        <HomeDeposit deposit={deposit} />
-        <HomeWithdrawal withdrawal={withdrawal} />
-        <HomeTradeOrder tradeOrder={tradeOrder} />
+      <main className="main-container">
+        <div className="input-group mb-3">
+          <select
+            onChange={(event) => setSelectedOption(event.target.value)}
+            className="custom-select"
+            id="inputGroupSelect02"
+          >
+            <option selected value="1">
+              Deposits
+            </option>
+            <option value="2">Trade Orders</option>
+            <option value="3">Withdrawals</option>
+          </select>
+        </div>
+        {selectedOption === "1" && <HomeDeposit deposit={deposit} />}
+        {selectedOption === "2" && <HomeWithdrawal withdrawal={withdrawal} />}
+        {selectedOption === "3" && <HomeTradeOrder tradeOrder={tradeOrder} />}
       </main>
     </div>
   );
