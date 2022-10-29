25c35ac8fa628d11547943a701550d6e3f585911
Catalin <IancuCatalinCristian94@gmail.com>
2022-10-29 19:57:19 +0300

    Installing Bootstrap

--- domains/home/components/homeDeposit/homeDeposit.tsx
+++ domains/home/components/homeDeposit/homeDeposit.tsx
@@ -6,7 +6,7 @@ const HomeDeposit: FC<Props> = ({ deposit }) => {
   console.log("Suntem aici " + deposit);
   return (
     <div>
-      {deposit?.map((element, i) => {
+      {deposit.map((element, i) => {
         return (
           <div key={i}>
             <h1>Amount: {element.amount}</h1>
@@ -14,6 +14,37 @@ const HomeDeposit: FC<Props> = ({ deposit }) => {
           </div>
         );
       })}
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
   //return <></>;
