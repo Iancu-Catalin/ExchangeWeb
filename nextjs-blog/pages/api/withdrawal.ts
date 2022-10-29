ace22127439efae7eecbb0a64be6e673afb29708
Catalin <IancuCatalinCristian94@gmail.com>
2022-10-29 15:12:06 +0300

    Cleanup and withdrawal.ts adding

---
+++ nextjs-blog/pages/api/withdrawal.ts
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "GET") {
+    res.status(400).json({});
+    return;
+  }
+  const fetchRes = await fetch("http://localhost:7118/Withdrawal", {
+    method: "GET",
+  });
+  res.status(200).send(await fetchRes.text());
+};
+
+export default handler;
